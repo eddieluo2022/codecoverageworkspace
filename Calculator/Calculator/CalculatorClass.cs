14b859b5111800bf9da5f08b746e283d9142390d
Luo <i37238@air-worldwide.com>
2022-11-28 17:16:21 -0500

    Add Calculator project

---
+++ Calculator/Calculator/CalculatorClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class CalculatorClass
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Calculator/CalculatorTest/CalculatorTestClass.cs
@@ -0,0 +1,38 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+
+namespace CalculatorTest
+{
+    [TestClass]
+    public class CalculatorTestClass
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            CalculatorClass c = new CalculatorClass();
+            Assert.AreEqual(10, c.Add(5, 5));
+        }
+
+        [TestMethod]
+        public void TestSubtract()
+        {
+            CalculatorClass c = new CalculatorClass();
+            Assert.AreEqual(0, c.Subtract(5, 5));
+        }
+
+        [TestMethod]
+        public void TestMultiply()
+        {
+            CalculatorClass c = new CalculatorClass();
+            Assert.AreEqual(25, c.Multiply(5, 5));
+        }
+
+        [TestMethod]
+        public void TestDivide()
+        {
+            //CalculatorClass c = new CalculatorClass();
+            //Assert.AreEqual(1, c.Divide(5, 5));
+            Assert.Fail();
+        }
+    }
+}
